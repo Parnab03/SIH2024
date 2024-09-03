a09ec303afb713e030c4e48058d7d97769e6817d
Soumik Ghosh <118295198+SoumikGhosh9@users.noreply.github.com>
2024-09-03 21:50:20 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Application from "./Components/Application";
+
+function App() {
+  return (
+    <>
+      <Application></Application>
+    </>
+  );
+}
+
+export default App;
