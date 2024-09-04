b35fe0136773842757f64760a60bf16458d78855
Soumik Ghosh <118295198+SoumikGhosh9@users.noreply.github.com>
2024-09-04 18:40:39 +0530

    Add files via upload

--- Submitbutton.jsx
+++ Submitbutton.jsx
@@ -1,6 +1,22 @@
-function Submitbutton(){
-    return (
-        <button type="submit" className="btn btn-primary" justify-content = "center">Submit</button>
-    )
+function Submitbutton() {
+  return (
+    <button
+      type="submit"
+      className="btn btn-primary"
+      justify-content="center"
+      style={{
+        textAlign: "center",
+        height: "40px",
+        width: "200px",
+        borderRadius: "8px",
+        fontSize: "20px",
+        fontWeight: "bold",
+        color: "white",
+        background: "linear-gradient(to bottom,  #EFB506, #FFC107)",
+      }}
+    >
+      Submit
+    </button>
+  );
 }
 export default Submitbutton;
